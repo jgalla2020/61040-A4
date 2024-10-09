d9dbc0bb0396a2644639d35ae1094a272a22f34a
Jabes Gallardo <jgalla24@mit.edu>
2024-10-09 01:07:46 -0400

    Began to implement Profiling concept

--- server/concepts/profiling.ts
+++ server/concepts/profiling.ts
@@ -1,11 +1,33 @@
+import DocCollection, { BaseDoc } from "../framework/doc";
+
+export interface ProfileDoc extends BaseDoc {
+  name: string;
+  contact?: string;
+  bio?: string;
+}
+
 /**
  * concept: Profiling
  */
 export default class ProfilingConcept {
+  public readonly profiles: DocCollection<ProfileDoc>;
+
   /**
    * Make an instance of ProfilingConcept.
    */
   constructor(collectionName: string) {
+    this.profiles = new DocCollection<ProfileDoc>(collectionName);
+  }
+
+  async create() {
+    //
+  }
+
+  async view() {
+    //
+  }
+
+  async edit() {
     //
   }
 }
