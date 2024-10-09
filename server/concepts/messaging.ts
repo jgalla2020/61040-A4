5ec43318349f1a1d62ac505b07b4d8fbae9dc035
Jabes Gallardo <jgalla24@mit.edu>
2024-10-09 01:41:54 -0400

    Began to implement Messaging concept

--- server/concepts/messaging.ts
+++ server/concepts/messaging.ts
@@ -1,11 +1,45 @@
+import { ObjectId, Timestamp } from "mongodb";
+import DocCollection, { BaseDoc } from "../framework/doc";
+
+export interface MessageDoc extends BaseDoc {
+  sender: ObjectId;
+  recipient: ObjectId;
+  time: Timestamp;
+  message: string;
+}
+
 /**
  * concept: Messaging
  */
 export default class MessagingConcept {
+  public readonly drafts: DocCollection<MessageDoc>;
+  public readonly sent: DocCollection<MessageDoc>;
+
   /**
    * Make an instance of MessagingConcept.
    */
   constructor(collectionName: string) {
+    this.drafts = new DocCollection<MessageDoc>(collectionName + "_drafted");
+    this.sent = new DocCollection<MessageDoc>(collectionName + "_sent");
+  }
+
+  async create() {
+    //
+  }
+
+  async view() {
+    //
+  }
+
+  async delete() {
+    //
+  }
+
+  async edit() {
+    //
+  }
+
+  async send() {
     //
   }
 }
