44a4d343593fe6c9970bc83c7c87ca209779b13d
Jabes Gallardo <jgalla24@mit.edu>
2024-10-09 01:33:24 -0400

    Began to implement Tracking concept

--- server/concepts/tracking.ts
+++ server/concepts/tracking.ts
@@ -1,11 +1,42 @@
+import { ObjectId } from "mongodb";
+import DocCollection, { BaseDoc } from "../framework/doc";
+
+export type GoalStatus = "complete" | "pending" | "past due";
+
+export interface GoalDoc extends BaseDoc {
+  author: ObjectId;
+  name: string;
+  description: string;
+  status: GoalStatus;
+  due: Date;
+}
+
 /**
  * concept: Tracking
  */
 export default class TrackingConcept {
+  public readonly goals: DocCollection<GoalDoc>;
+
   /**
    * Make an instance of TrackingConcept.
    */
   constructor(collectionName: string) {
+    this.goals = new DocCollection<GoalDoc>(collectionName);
+  }
+
+  async create() {
+    //
+  }
+
+  async view() {
+    //
+  }
+
+  async delete() {
+    //
+  }
+
+  async edit() {
     //
   }
 }
