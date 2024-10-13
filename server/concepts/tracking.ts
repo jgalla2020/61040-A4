3cdfdeb577f6c898dda3b0d7968daa80b2afb1d0
Jabes Gallardo <jgalla24@mit.edu>
2024-10-13 18:09:28 -0400

    Added documentation to tracking.ts

--- server/concepts/tracking.ts
+++ server/concepts/tracking.ts
@@ -22,11 +22,22 @@ export default class TrackingConcept {
 
   /**
    * Make an instance of TrackingConcept.
+   *
+   * @param collectionName the name of this collection of goals.
    */
   constructor(collectionName: string) {
     this.goals = new DocCollection<GoalDoc>(collectionName);
   }
 
+  /**
+   * Create a goal with a title, description, and due date.
+   *
+   * @param executor the person working towards this goal.
+   * @param title the title of the goal.
+   * @param due the goal's expected completion date.
+   * @param description the goal's description.
+   * @returns an object containing a success message and the goal details.
+   */
   async create(executor: ObjectId, title: string, due: Date, description?: string) {
     const currentDate = new Date();
 
@@ -42,21 +53,51 @@ export default class TrackingConcept {
     return { msg: "Goal created successfully!", goal: await this.goals.readOne({ _id }) };
   }
 
-  async getGoal(_id: ObjectId) {
+  /**
+   * Get all of the goals created by some executor.
+   *
+   * @param executor the person with the goals.
+   * @returns an object containing a success message and all of the executor's goals.
+   */
+  async viewGoals(executor: ObjectId) {
+    const goals = await this.goals.readMany({ executor });
+
+    return { msg: `Successfully obtained all goals for executor ${executor}.`, goals: goals };
+  }
+
+  /**
+   * View goals with a particular status.
+   *
+   * @param executor the person with the goals.
+   * @param status the status of the goals.
+   * @returns an object containing a success message and the goals with the status.
+   */
+  async viewStatus(executor: ObjectId, status: GoalStatus) {
+    const goals = await this.goals.readMany({ executor, status });
+
+    return { msg: `Succesfully obtained goals with status ${status}.`, goals: goals };
+  }
+
+  /**
+   * Get a goal.
+   *
+   * @param _id the ID used to query the goal.
+   * @returns the goal identified by the ID.
+   */
+  async viewOneGoal(_id: ObjectId) {
     return await this.goals.readOne({ _id });
   }
 
-  async view(executor: ObjectId, status: GoalStatus) {
-    const goals = await this.goals.readMany({ executor, status });
-
-    return { msg: `Goals with status ${status}.`, goals: goals };
-  }
-
-  async delete(_id: ObjectId) {
-    await this.goals.deleteOne({ _id });
-    return { msg: "Goal deleted successfully!" };
-  }
-
+  /**
+   * Update a goal with new details.
+   *
+   * @param _id the ID used to query the goal.
+   * @param title the updated goal title (optional).
+   * @param description the updated goal description (optional).
+   * @param status the updated goal status (optional).
+   * @param due the updated goal due date (optional).
+   * @returns an object containing a success message and the updated goal details.
+   */
   async edit(_id: ObjectId, title?: string, description?: string, status?: GoalStatus, due?: Date) {
     const goal = await this.goals.readOne({ _id });
 
@@ -81,6 +122,22 @@ export default class TrackingConcept {
     return { msg: "Task updated successfully!", goal: await this.goals.readOne({ _id }) };
   }
 
+  /**
+   * Deletes a goal.
+   * @param _id the ID used to query the goal.
+   * @returns an object containing the goal deletion success message.
+   */
+  async delete(_id: ObjectId) {
+    await this.goals.deleteOne({ _id });
+    return { msg: "Goal deleted successfully!" };
+  }
+
+  /**
+   * Checks if some goal belongs to a user.
+   *
+   * @param _id the ID used to query the goal.
+   * @param user the ID for the user.
+   */
   async assertExecutorIsUser(_id: ObjectId, user: ObjectId) {
     const goal = await this.goals.readOne({ _id });
 
@@ -90,6 +147,25 @@ export default class TrackingConcept {
     if (goal.executor.toString() !== user.toString()) {
       throw new GoalExecutorNotMatchError(user, _id);
     }
+  }
+
+  /**
+   * Update the status of goals based on the current date and their due dates.
+   *
+   * @returns an object containing a success message for the goals being updated.
+   */
+  async updateGoalStatuses() {
+    const currentDate = new Date();
+
+    const pendingGoals = await this.goals.readMany({ status: "pending" });
+
+    for (const goal of pendingGoals) {
+      if (goal.due < currentDate) {
+        await this.goals.partialUpdateOne({ _id: goal._id }, { status: "past due" });
+      }
+    }
+
+    return { msg: "The goal statuses have been updated based on the current date." };
   }
 }
 
