5d9c2202e7b946154dcb715eba7b5b91ddb2beee
Jabes Gallardo <jgalla24@mit.edu>
2024-10-13 17:37:21 -0400

    Added documentation to profiling.ts

--- server/concepts/profiling.ts
+++ server/concepts/profiling.ts
@@ -18,27 +18,47 @@ export default class ProfilingConcept {
 
   /**
    * Make an instance of ProfilingConcept.
+   *
+   * @param collectionName the name of this collection of profiles.
    */
   constructor(collectionName: string) {
     this.profiles = new DocCollection<ProfileDoc>(collectionName);
   }
 
+  /**
+   * Create a profile with a user's name, contact, and bio.
+   *
+   * @param user the user this profile belongs to.
+   * @param name the name of the user.
+   * @param contact the user's contact.
+   * @param bio a description of the user.
+   * @returns an object containing a success message and the profile details.
+   */
   async create(user: ObjectId, name: string, contact?: string, bio?: string) {
     await this.profiles.createOne({ user, name, contact, bio });
 
     return { msg: "Profile created successfully!", profile: await this.profiles.readOne({ user }) };
   }
 
+  /**
+   * Get the profile belonging to some user.
+   *
+   * @param user the user viewing the profile.
+   * @returns the profile created by this user.
+   */
   async viewProfile(user: ObjectId) {
     return await this.profiles.readOne({ user });
   }
 
-  async delete(user: ObjectId) {
-    await this.profiles.deleteOne({ user });
-
-    return { msg: "Profile deleted successfully!" };
-  }
-
+  /**
+   * Update a user's profile details.
+   *
+   * @param user the user updating the profile (required).
+   * @param name the updated name (optional).
+   * @param contact the updated contact information (optional).
+   * @param bio the updated bio (optional).
+   * @returns an object containing a success message and the updated profile details.
+   */
   async update(user: ObjectId, name?: string, contact?: string, bio?: string) {
     const userProfile = await this.viewProfile(user);
 
@@ -59,6 +79,22 @@ export default class ProfilingConcept {
     return { msg: "Profile updated successfully!", profile: await this.profiles.readOne({ user }) };
   }
 
+  /**
+   *
+   * @param user the user deleting the profile.
+   * @returns an object containing the profile deletion success message.
+   */
+  async delete(user: ObjectId) {
+    await this.profiles.deleteOne({ user });
+
+    return { msg: "Profile deleted successfully!" };
+  }
+
+  /**
+   * Checks if some user's profile exists.
+   *
+   * @param user the profile's user.
+   */
   async assertProfileExists(user: ObjectId) {
     const profile = await this.profiles.readOne({ user });
 
@@ -67,6 +103,11 @@ export default class ProfilingConcept {
     }
   }
 
+  /**
+   * Checks if some user's profile does not exist.
+   *
+   * @param user the profile's user.
+   */
   async assertProfileDoesNotExist(user: ObjectId) {
     const profile = await this.profiles.readOne({ user });
 
