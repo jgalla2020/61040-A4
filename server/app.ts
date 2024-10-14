5d8762bc2a40fec50cd72d12ffa28d2c26f87e37
Jabes Gallardo <jgalla24@mit.edu>
2024-10-13 23:31:16 -0400

    Final concept instantiations

--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,11 @@
 import AuthenticatingConcept from "./concepts/authenticating";
 import FriendingConcept from "./concepts/friending";
+import ItemizingConcept from "./concepts/itemizing";
 import MessagingConcept from "./concepts/messaging";
 import PostingConcept from "./concepts/posting";
 import PreferencesConcept from "./concepts/preferences";
 import ProfilingConcept from "./concepts/profiling";
 import SessioningConcept from "./concepts/sessioning";
-import SharingConcept from "./concepts/sharing";
-import TaskingConcept from "./concepts/tasksetting";
 import TrackingConcept from "./concepts/tracking";
 
 // The app is a composition of concepts instantiated here
@@ -17,9 +16,8 @@ export const Posting = new PostingConcept("posts");
 export const Friending = new FriendingConcept("friends");
 
 // These are the concepts that I added for my app:
-export const Profiling = new ProfilingConcept("profiles");
-export const Preferences = new PreferencesConcept("preferences");
-export const Tasking = new TaskingConcept("tasks");
+export const Itemizing = new ItemizingConcept("items");
 export const Tracking = new TrackingConcept("goals");
+export const Profiling = new ProfilingConcept("profiles");
 export const Messaging = new MessagingConcept("messages");
-export const Sharing = new SharingConcept("content");
+export const Preferences = new PreferencesConcept("preferences");
